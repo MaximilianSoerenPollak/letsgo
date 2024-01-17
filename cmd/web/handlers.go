d8061962e52f76d4dc7664141ff16a8d89253aab
Maximilian <74557518+MaximilianSoerenPollak@users.noreply.github.com>
2024-01-17 06:31:35 +0000

    Added application struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,9 @@ import (
 )
 
 // ==== ROUTES ====
-func home(w http.ResponseWriter, r *http.Request) {
+// Change signature of the home handler so it is defined against *application
+// We basically make this a method of Application instead of being it's own function.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r) // This function replies to the request with a 404 not found error.
 		return
@@ -32,18 +34,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+	  	// Because the home handler is now a method against the application
+        // struct it can access its fields, including the structured logger. We'll 
+        // use this to create a log entry at Error level containing the error
+        // message, also including the request method and URI as attributes to 
+        // assist with debugging.
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "Interval Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -53,7 +60,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)                         // Leeting user know what methods are allowed once we deny his one
 		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed) // Letting the user know that the used method is not correct
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+
+type application struct {
+	logger *slog.Logger 
+}
+
 func main() {
+	// ===== Command line Arguments =====
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	flag.Parse()
+
+	// ===== Create Custom Logger =====
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	
+	// ===== Initialize Application struct =====
+	// initialize a new instance of the application struct with all dependencies (e.g. our logger for now)
+	app := &application{
+		logger: logger,
+	}
+
+	// ===== Start & Config Server and routes =====
+
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	logger.Info("starting server", slog.String("addr", ":4000"))
+	// Using the logger to return / log any errors that http.ListenAndServe gives us.
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 
 }
