4f8c1723a24756bb4421470cfb694a94e4edfa3d
Maximilian <74557518+MaximilianSoerenPollak@users.noreply.github.com>
2024-01-23 10:15:49 +0000

    Added human readable date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -29,7 +29,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
@@ -48,5 +48,9 @@ func newTemplateCache() (map[string]*template.Template, error) {
 }
 
 func humanDate(t time.Time) string{
-	return t.Format("02 Jan 2006 at 15:40")
+	return t.Format("02 Jan 2006 at 15:43 ")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
