050374bd965a1a123771da4486608d6210d4e292
Maximilian <74557518+MaximilianSoerenPollak@users.noreply.github.com>
2024-02-11 21:53:35 +0000

    Made some more helper funcs

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,43 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-
 
 	"snippetbox.msp.net/internal/assert"
 )
 
-
 func TestPing(t *testing.T) {
 	
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
 	// Initlaize a new HTTPTEST Response recorder
-	rr := httptest.NewRecorder()
+	defer ts.Close()
 
-	// Init a dummy 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Call the route we want to test.
-	ping(rr, r)
-	
-	// Grab the result from the ping 
-	rs := rr.Result() 
+	code, _, body := ts.get(t, "/ping")
 
-	// see if we get a 200 code back
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 
-	defer rs.Body.Close()
-
-	// Read the body of the response 
-	body,err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,26 +17,23 @@ func (app *application) routes() http.Handler {
 	})
 	fileServer := http.FileServer(http.FS(ui.Files))
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
-	
+
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-
 
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
-
-
 	protected := dynamic.Append(app.requireAuthentication)
 
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
-
 
 	// Making our standard middlewear chain here that recovers pani, logs and sends secure headers.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
